e6a34002f22a79cd14fdf4ef348461cba3ca470c
Pavel Belousov <signorpao@gmail.com>
2024-02-20 18:15:33 +0300

    faq...

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__container .header__flex", {
+  ...scrollRevealOption,
+  delay: 750,
+});
